f06a6dc7a3126328325516ad48153ecfe4f60d00
Mateus Sales Lima Silva <Mateus50502566@edu.df.senac.br>
2024-10-22 12:22:40 -0300

    bolinha

--- Assets/Scripts/Movimentacao.cs
+++ Assets/Scripts/Movimentacao.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Movimentacao : MonoBehaviour
@@ -11,13 +12,18 @@ public class Movimentacao : MonoBehaviour
     [SerializeField]private float forcaPulo;
     [SerializeField]private bool invertH;
     [SerializeField]private bool invertV;
-    [SerializeField]private int pontos;
-    [SerializeField]private bool estaVivo;
+    [Header("Sons da Bolinha")]
+    [SerializeField] private AudioClip pulo;
+    [SerializeField] private AudioClip pegaCubo;
+    [SerializeField] private AudioSource audioPlayer;
+    [SerializeField]public static int pontos;
+    [SerializeField]public static bool estaVivo;
     // Start is called before the first frame update
     void Start()
     {
         estaVivo = true;
         rb = GetComponent<Rigidbody>();
+        audioPlayer = GetComponent<AudioSource>();
     
     }
 
@@ -28,7 +34,7 @@ public class Movimentacao : MonoBehaviour
         {
             moveV = Input.GetAxis("Vertical");
             moveH = Input.GetAxis("Horizontal");
-            transform.position += new Vector3(moveH * Time.deltaTime, 0, moveV * Time.deltaTime );
+            transform.position += new Vector3(moveH * velocidade * Time.deltaTime, 0, moveV * velocidade *Time.deltaTime );
 
             //Pulo
             if(Input.GetKeyDown(KeyCode.Space))
@@ -38,16 +44,21 @@ public class Movimentacao : MonoBehaviour
         }
         
     }
-    private void OnTriggerEnter(Collider other)
+    
+    private void OnTriggerEnter(Collider cubo)
     {
-        Destroy(this.gameObject);
+        Destroy(cubo.gameObject);
+        audioPlayer.PlayOneShot(pegaCubo);
+        pontos++;
     }
-    private void OnColisionEnter(Collision other)
+
+    private void OnCollisionEnter(Collision Lava)
     {
-        if(other.gameObject.CompareTag("Lava"))
+        if(Lava.gameObject.CompareTag("Lava"))
         {
             estaVivo = false;
             Time.timeScale = 0;
         }
     }
+
 }
---
+++ Assets/Scripts/Textos.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Textos : MonoBehaviour
+{
+    public TextMeshProUGUI textoPontos;
+    public TextMeshProUGUI textoDeveres;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AtualizarTexto();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AtualizarTexto();
+    }
+
+    void AtualizarTexto()
+    {
+        textoPontos.text = Movimentacao.pontos.ToString() + "/16";
+        if (Movimentacao.pontos == 0)
+        {
+            textoDeveres.text = ("Colete todos os cubos para passar de fase");
+        }
+        if (Movimentacao.pontos == 8)
+        {
+            textoDeveres.text = ("Só mais um pouco");
+        }
+        if (Movimentacao.pontos == 16)
+        {
+            textoDeveres.text = ("Vá para o Portal!");
+        }
+        if (Movimentacao.estaVivo == false)
+        {
+            textoDeveres.text =("Você Morreu!!");
+        }
+    }
+}
